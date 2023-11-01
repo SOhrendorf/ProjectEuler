e6ad37b529a8ef46f7adc6bb8f8c5c08300d1038
SOhrendorf <74784123+SOhrendorf@users.noreply.github.com>
2023-11-01 22:15:14 +0100

    make it work :)

--- src/SumSquareDifference.java
+++ src/SumSquareDifference.java
@@ -1,29 +1,23 @@
 public class SumSquareDifference {
     public static void main(String[] args) {
-        System.out.println(sum(10));
-        System.out.println(factor(10));
         int solution = sum(10) - factor(10);
         System.out.println(solution);
     }
 
     public static int factor(int range){
-        int fac = 1;
-        for (int i = 0; i < range; i++){
-            fac = fac*fac;
-            fac++;
+        int result;
+        result = 0;
+        for(int i = 1; i <= range; i++){
+            result += i*i;
         }
-        return fac;
+        return result;
     }
 
     public static int sum(int range){
         int sum = 0;
         for (int i = 0; i < range; i++){
-            i++;
-            sum = sum + i;
-            System.out.println(sum);
+            sum += i+1;
         }
-        System.out.println("summe: " + sum);
         return sum*sum;
     }
 }
-
